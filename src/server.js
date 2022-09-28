6c5af38dbfc8fcfaab848a20018494f8ecbfa3be
Jonathan <aguss997779@gmail.com>
2022-09-27 23:27:26 -0300

    fix loggers VI

--- src/server.js
+++ src/server.js
@@ -42,83 +42,82 @@ const numCPUs = os.cpus().length
 
 const MODE = args.MODE === "CLUSTER"
 
-if (cluster.isMaster && MODE) {
-    console.log(`Master (${process.pid}) is running...`);
-    console.log(`Numero de procesadores: ${numCPUs}`)
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork()
-    }
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`Worker ${worker.process.pid} died`)
-        cluster.fork()
-    })
-} else {
+// if (cluster.isMaster && MODE) {
+//     console.log(`Master (${process.pid}) is running...`);
+//     console.log(`Numero de procesadores: ${numCPUs}`)
+//     for (let i = 0; i < numCPUs; i++) {
+//         cluster.fork()
+//     }
+//     cluster.on('exit', (worker, code, signal) => {
+//         console.log(`Worker ${worker.process.pid} died`)
+//         cluster.fork()
+//     })
+// } else {
 
-    const app = express()
+const app = express()
 
-    const httpServer = http.createServer(app)
-    const io = new ioServer(httpServer)
-
-
-    app.set("view engine", "ejs")
-    app.set("views", __dirname + "/public/views")
-
-    // middlewares
-    app.use(express.json())
-    app.use(express.static(__dirname + "/public"))
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(session(
-        {
-            secret: 'secret',
-            resave: true,
-            saveUninitialized: true,
-            store: MongoStore.create({
-                mongoUrl: `mongodb+srv://jonathan:mongo753@clustercab.kfxke.mongodb.net/session2?retryWrites=true&w=majority`,
-                ttl: 60 * 10
-            })
-        }
-    ))
-    app.use(passport.initialize())
-    app.use(passport.session())
-    app.use("/login", rutaLogin)
-    app.use("/logout", rutaLogout)
-    app.use("/register", rutaRegistro)
-    app.use("/", rutaProductos)
-    app.use("/errorLogin", rutaErrorLogin)
-    app.use("/errorRegister", rutaErrorRegister)
-    app.use("/info", rutaInfo)
-    app.use("/api/randoms", rutaApi)
+const httpServer = http.createServer(app)
+const io = new ioServer(httpServer)
 
 
+app.set("view engine", "ejs")
+app.set("views", __dirname + "/public/views")
 
-
-
-    // io
-    io.on("connection", (socket) => {
-        console.log("conectado")
-        // socket.emit("productos", container.getAll())
-
-        // socket.on("newProd", (arg) => {
-        //     console.log(arg)
-        //     io.sockets.emit("productos", container.getAll())
-        // })
-        let msjDB = new MsjContainer()
-
-        const msj = msjDB.getAll()
-
-        socket.emit("chat", msj)
-
-        socket.on("newMsj", newMsj => {
-            msjDB.save(newMsj)
-            io.sockets.emit("chat", msj)
+// middlewares
+app.use(express.json())
+app.use(express.static(__dirname + "/public"))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session(
+    {
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true,
+        store: MongoStore.create({
+            mongoUrl: `mongodb+srv://jonathan:mongo753@clustercab.kfxke.mongodb.net/session2?retryWrites=true&w=majority`,
+            ttl: 60 * 10
         })
-    })
+    }
+))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use("/login", rutaLogin)
+app.use("/logout", rutaLogout)
+app.use("/register", rutaRegistro)
+app.use("/", rutaProductos)
+app.use("/errorLogin", rutaErrorLogin)
+app.use("/errorRegister", rutaErrorRegister)
+app.use("/info", rutaInfo)
+app.use("/api/randoms", rutaApi)
 
-    // server
-    const PORT = args.PORT || process.env.PORT
-    const server = httpServer.listen(PORT, () => {
-        console.log(`Servidor iniciado en el puerto ${server.address().port} ( http://localhost:${server.address().port} ) PID: ${process.pid}`)
-    })
 
-    server.on("error", error => console.log(error))
-}
+
+
+
+// io
+io.on("connection", (socket) => {
+    console.log("conectado")
+    // socket.emit("productos", container.getAll())
+
+    // socket.on("newProd", (arg) => {
+    //     console.log(arg)
+    //     io.sockets.emit("productos", container.getAll())
+    // })
+    let msjDB = new MsjContainer()
+
+    const msj = msjDB.getAll()
+
+    socket.emit("chat", msj)
+
+    socket.on("newMsj", newMsj => {
+        msjDB.save(newMsj)
+        io.sockets.emit("chat", msj)
+    })
+})
+
+// server
+const PORT = args.PORT || process.env.PORT
+const server = httpServer.listen(PORT, () => {
+    console.log(`Servidor iniciado en el puerto ${server.address().port} ( http://localhost:${server.address().port} ) PID: ${process.pid}`)
+})
+
+server.on("error", error => console.log(error))
